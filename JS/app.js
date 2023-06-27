37a905d6b677b98c66e6b0231815f0912bcd311f
sebaespinoza01e <espinoza1990@me.como>
2023-06-27 15:38:36 -0400

    avances segunda pre entrega codert

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,59 @@
-// Función que verifica si un número es par o impar
-function esPar(numero) {
-    if (numero % 2 === 0) {
-        return true;
-    } else {
-        return false;
+let nombreEntrenador = prompt("¡Bienvenido, entrenador Pokémon! ¿Cuál es tu nombre?");
+alert("¡Hola, " + nombreEntrenador + "! Prepárate para comenzar tu aventura Pokémon.");
+
+class Producto{
+    constructor(id, nombre, precio, description){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.description = description;
     }
 }
-  // Bucle que muestra los números del 1 al 10 y verifica si son pares o impares
-for (let i = 1; i <= 10; i++) {
-    if (esPar(i)) {
-        alert(i + " es par");
-    } else {
-        alert(i + " es impar");
-    }
-}
+//productos del listado
+const productos = [
+    new Producto (1, 'Bulbasaur', 200, 'Pokemon tipo hierva'),
+    new Producto (2, 'Ivysaur', 250, 'Pokemon tipo hierva'),
+    new Producto (3, 'Venusaur', 300, 'Pokemon tipo hierva'),
+    new Producto (4, 'Charmander', 200, 'Pokemon tipo fuego'),
+    new Producto (5, 'Charmeleon', 250, 'Pokemon tipo fuego'),
+    new Producto (6, 'Charizard', 300, 'Pokemon tipo fuego'),
+    new Producto (7, 'Squirtle', 200, 'Pokemon tipo agua'),
+    new Producto (8, 'Wartortle', 250, 'Pokemon tipo agua'),
+    new Producto (9, 'Blastoise', 300, 'Pokemon tipo agua'),
+    new Producto (10, 'Caterpie', 200, 'Pokemon tipo bicho'),
+    new Producto (11, 'Metapod', 250, 'Pokemon tipo bicho'),
+    new Producto (12, 'Butterfree', 300, 'Pokemon tipo bicho'),
+    new Producto (13, 'Weedle', 200, 'Pokemon tipo bicho'),
+    new Producto (14, 'Kakuna', 250, 'Pokemon tipo bicho'),
+    new Producto (15, 'Beedrill', 300, 'Pokemon tipo bicho'),
+    new Producto (16, 'Pidgey', 200, 'Pokemon tipo normal'),
+    new Producto (17, 'Pidgeotto', 250, 'Pokemon tipo normal'),
+    new Producto (18, 'Pidgeot', 300, 'Pokemon tipo normal'),
+    new Producto (19, 'Rattata', 200, 'Pokemon tipo normal'),
+    new Producto (20, 'Raticate', 300, 'Pokemon tipo normal'),
+    new Producto (21, 'Spearow', 200, 'Pokemon tipo normal'),
+    new Producto (22, 'Fearow', 300, 'Pokemon tipo normal'),
+    new Producto (23, 'Ekans', 200, 'Pokemon tipo veneno'),
+    new Producto (24, 'Arbok', 300, 'Pokemon tipo veneno'),
+    new Producto (25, 'Pikachu', 400, 'Pokemon tipo electrico'),
+];
+//tomamos el div tipo container del html
+const productContainer = document.querySelector('#product-container');
+//div de la card, para dar forma
+productos.forEach(producto => {
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('producto');
+    //innerHtml 
+    productDiv.innerHTML = `
+        <div class='card'>
+            <h3>${producto.id}</h3>
+            <h1>${producto.nombre}</h1>
+            <p>${producto.precio}</p>
+            <p>${producto.description}</p>
+            <button> "Agregar-a-tu-Pokebola" </button>
+            </div>
+            `;
+    productContainer.appendChild(productDiv);
+
+
+})
